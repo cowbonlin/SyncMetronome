576207072ff78b1d6fb23bedec5aec3673dc6415
cowbon <alexalex850520@gmail.com>
2019-02-16 17:01:10 +0800

    move main function from App.js to Bpm.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,45 +1,10 @@
 import React from 'react';
-import Tone from 'tone';
 
 import Bpm from './Bpm';
 import JamRoom from './JamRoom';
 import '../styles/Main.css';
 
-class App extends React.Component {
-    state = { playing: false, bpm: 60 };
-    
-    componentDidMount() {
-        const option = { 
-            oscillator: { type  : 'sine' },
-            envelope: {
-                attack: 0.005,
-                decay: 0.1,
-                sustain: 0.03,
-                release: 0.1
-            }
-        };
-        const synth = new Tone.Synth(option).toMaster();
-        Tone.Transport.bpm.value = this.state.bpm;
-        Tone.Transport.scheduleRepeat( time => {
-            synth.triggerAttackRelease('C4', '0.1')
-        }, '4n');
-    }
-    
-    onPlayClicked = () => {
-        if (this.state.playing) {
-            Tone.Transport.stop();
-        }
-        else {
-            Tone.Transport.start();
-        }
-        this.setState({ playing: !this.state.playing });
-    }
-    
-    onBpmChange = bpm => {
-        this.setState({bpm: bpm});
-        Tone.Transport.bpm.value = bpm;
-    }
-    
+class App extends React.Component {    
     render() {
         return (
             <div className="ui fluid textcontainer" id="app-container">
@@ -49,29 +14,13 @@ class App extends React.Component {
                     </div>
                     
                     <div className="eight wide column">
-                        <div className="ui fluid centered card" id="main-card">
+                        <Bpm />
+                    </div>
+                    
+                    <div className="four wide column">
+                        <div className="ui fluid card">
                             <div className="content">
-                                <h1 className="ui dividing header">Metronome</h1>
-                                
-                                <Bpm onBpmChange={this.onBpmChange} bpm={this.state.bpm} />
-                                    
-                                <button className="massive circular ui icon teal button" onClick={this.onPlayClicked} style={{marginTop: '40px'}}>
-                                    <i className={`icon ${this.state.playing? 'pause':'play' }`}></i>
-                                </button>
-                                    
-                                <div className="ui three basic blue icon buttons" style={{marginTop: '40px'}}>
-                                    <button className="ui button">
-                                        <i className="music icon"></i>
-                                    </button>
-                                    <button className="ui button">
-                                        <i className="wifi icon"></i>
-                                    </button>
-                                    <button className="ui button">
-                                        <i className="cog icon"></i>
-                                    </button>
-                                    
-                                </div>
-                                <button onClick={()=> {console.log(this.state)}}>hitme</button>
+                                left card
                             </div>
                         </div>
                     </div>
--- client/src/components/Bpm.js
+++ client/src/components/Bpm.js
@@ -1,6 +1,36 @@
 import React from 'react';
+import Tone from 'tone';
 
 class Bpm extends React.Component {
+    state = { playing: false, bpm: 60 };
+    
+    componentDidMount() {
+        const option = { 
+            oscillator: { type  : 'sine' },
+            envelope: {
+                attack: 0.005,
+                decay: 0.1,
+                sustain: 0.03,
+                release: 0.05
+            }
+        };
+        const synth = new Tone.Synth(option).toMaster();
+        Tone.Transport.bpm.value = this.state.bpm;
+        Tone.Transport.scheduleRepeat( time => {
+            synth.triggerAttackRelease('C5', '0.1')
+        }, '4n');
+    }
+    
+    onPlayClicked = () => {
+        if (this.state.playing) {
+            Tone.Transport.stop();
+        }
+        else {
+            Tone.Transport.start();
+        }
+        this.setState({ playing: !this.state.playing });
+    }
+    
     onBpmChange = bpmStr => {
         if (bpmStr === '') {
             bpmStr = '0';
@@ -8,25 +38,39 @@ class Bpm extends React.Component {
         const bpm = parseInt(bpmStr, 10);
         if (!isNaN(bpmStr) && bpm >= 0) {
             const bpm = parseInt(bpmStr, 10);
-            this.props.onBpmChange(bpm);
+            this.setState({bpm: bpm});
+            Tone.Transport.bpm.value = bpm;
         }
     }
-    
+
     render() {
         return (
-            <div className="ui center aligned grid">
-                <div className="eight wide column">
-                    <div className="ui massive input">
-                        <input 
-                            type="text" 
-                            value={this.props.bpm}
-                            onChange={ e => { this.onBpmChange(e.target.value) }}
-                            style={{width: '100px'}} 
-                        />
+            <div className="ui fluid centered card" id="main-card">
+                <div className="content">
+                    <h1 className="ui dividing header">Metronome</h1>
+                    <div className="ui center aligned grid">
+                        <div className="row">
+                            <div className="eight wide column">
+                                <div className="ui massive input">
+                                    <input 
+                                        type="text" 
+                                        value={this.state.bpm}
+                                        onChange={ e => { this.onBpmChange(e.target.value) }}
+                                        style={{width: '100px'}} 
+                                    />
+                                </div>
+                                <div className="description">BPM</div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <button className="massive circular ui icon teal button" onClick={this.onPlayClicked} style={{marginTop: '40px'}}>
+                                <i className={`icon ${this.state.playing? 'pause':'play' }`}></i>
+                            </button>
+                        </div>
                     </div>
-                    <div className="description">BPM</div>
                 </div>
             </div>
+                
         );
     }
 }
