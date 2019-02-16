a003c9794325e8b3ae57f91b576dd12e9c6c0a9e
cowbon <alexalex850520@gmail.com>
2019-02-16 18:28:41 +0800

    apply short uuid on server

--- server/app.py
+++ server/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
+from shortuuid import ShortUUID
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'hello world'
+
+@app.route('/room/create')
+def create_new_room():
+    new_room_code = ShortUUID().random(length=5)
+    return jsonify(roomcode=new_room_code)
+
 
 if __name__ == '__main__':
     app.run()
